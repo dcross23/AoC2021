a689f2019ff2d2ef80f1882898d0ef713aa4f163
David Cruz García <david.cruz.garciaa@gmail.com>
2022-01-12 12:59:26 +0100

    Fix

--- src/Day12/graph.h
+++ src/Day12/graph.h
@@ -29,7 +29,14 @@ GNode* createGNode(char *name);
 int findNode(char *name, Graph *g);
 int addGNode(GNode *node, Graph *g);
 void addLineNodes(char *name, Graph *g, char *nextName);
+void printGraph(Graph *g);
 
+int dfs(GNode *u, GNode *v);
+int findNumPaths(Graph *g);
 
+int isInArray(char *s, char (*array)[SSIZE], int sz);
+bool checkSmallCaveRepeated(Graph *g, char (*currentPath)[SSIZE], int idx);
+int dfs2(GNode *u, GNode *v, Graph *g, char (*currentPath)[SSIZE], int *idx);
+int findNumPaths2(Graph *g);
 
 #endif
